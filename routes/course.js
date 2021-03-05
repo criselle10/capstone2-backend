d3fd2c8e364bd51e7452cffde386cd80875d646f
criselle albor <alborcriselle10@gmail.com>
2021-03-05 13:31:17 +0800

    create update course feature

--- routes/course.js
+++ routes/course.js
@@ -7,13 +7,25 @@ router.post('/', (req, res) => {
     CourseController.add(req.body).then(course => res.send(course))
 });
 
-//get course
+//get all course
 router.get('/', (req, res) => {
     CourseController.getAll().then(courses => res.send(courses))
 });
 
+//delete course
 router.delete('/:courseId', (req, res) => {
     CourseController.archive(req.params.courseId).then(course => res.send(course))
 })
+
+//get specific course
+router.get('/:courseId', (req, res) => {
+    CourseController.get(req.params.courseId).then(course => res.send(course))
+});
+
+//update course
+router.put('/', (req, res) => {
+    CourseController.update(req.body).then(result => res.send(result))
+})
+
 
 module.exports = router;
