8853c07dc75c8742fee86eb1b2b7c0b821c26013
criselle albor <alborcriselle10@gmail.com>
2021-03-08 11:03:42 +0800

    details activity

--- routes/user.js
+++ routes/user.js
@@ -18,9 +18,17 @@ router.post('/login', (req, res) => {
 })
 
 
-
 // details
 // return the user details of the logged user
+// router.get('/', (req, res) => {
+//     UserController.find({ isActive: true })
+// })
+router.get('/details', (req, res) => {
+    let token = req.headers.authorization.replace("Bearer ", "");
+    let decoded = jwt.verify(token, process.env.SECRET);
+    User.findById(decoded.id, { password: 0 })
+        .then(user => res.send(user))
+});
 
 // enroll
 // enroll student
